7398e9c9213051721c44217b94c676dc7385ceeb
mood <chamesh@naver.com>
2022-12-02 14:07:52 +0900

    feat : footer 영역잡기

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,3 @@
-// 마우스 오버를 할 때마다 해당 div에 on이 붙어야 함
-// div를 객체로 만들기? -> div[i] 선택될 때마다 on이 붙음
-// 그러면 div[i] 이외의 것들에는 전부 remove 먹여줘야 함
-// 해당 인덱스 객체 하나 만들고, 그 값이랑 맞으면 add on, 안맞으면 remove on?
-
-// 이게 돼?,,,,,,,,
-
-//mouseover 걸어줄 엘리먼트
-//
-
-// const gnbElement = document.querySelector(".gnb");
-// const gnbs = document.querySelectorAll(".gnb>li>.sub");
 const header = document.querySelector("#header");
 const lis = document.querySelectorAll(".gnb>li")
 const divElement = document.querySelectorAll(".gnb>li>.sub")
@@ -21,7 +9,6 @@ for (let i = 0; i < lis.length; i++) {
   header.addEventListener("mouseout", function (e) {
     divElement[i].classList.remove("on");
   });
-
 }
 
 // header 네비게이터
